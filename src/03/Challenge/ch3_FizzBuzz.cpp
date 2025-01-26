17e70296ced2d247c1820f882b3cc7aae8634cd5
Sachindu Malshan <cst19035@std.uwu.ac.lk>
2025-01-26 12:19:57 +0000

    Fizz Buzz problem solved

--- src/03/Challenge/ch3_FizzBuzz.cpp
+++ src/03/Challenge/ch3_FizzBuzz.cpp
@@ -15,7 +15,12 @@ int main(){
     std::cout << "Enter a positive integer: " << std::flush;
     std::cin >> n;
 
-    // Write your code here
+    for(int i=1; i<n+1; i++){
+        if(i%3 ==0 and i%5 == 0) std::cout << "Fizz Buzz" << std::endl;
+        else if(i%3 ==0) std::cout << "Fizz" << std::endl;
+        else if(i%5 == 0) std::cout << "Buzz" << std::endl;
+        else std::cout << i << std::endl;
+    }
     
     std::cout << std::endl << std::flush;
     return 0;
