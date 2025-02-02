9b2b9b8e058bf126b8447263e4326ddf7bc0f3b7
Sachindu Malshan <cst19035@std.uwu.ac.lk>
2025-02-02 12:36:43 +0000

    Solved the problem in a another way without using any library

--- src/08/Challenge/ch8_ip_address.cpp
+++ src/08/Challenge/ch8_ip_address.cpp
@@ -9,7 +9,6 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <sstream>
 
 // is_valid_ip()
 // Summary: This function validates an IP address.
@@ -17,15 +16,34 @@
 //           ip: The string to analyze with a potential ip address.
 // Returns: A boolean value. True for valid ip addresses, false otherwise.
 bool is_valid_ip(std::string ip){
-    char delimeter = '.';
-    std::string ip_node;
+    std::vector <std::string> ip_list;
+    std::vector <char> tempc;
+    int prev =0;
 
-    std::stringstream stream_text(ip);
-    while(getline(stream_text,ip_node,delimeter)){
-        if(stoi(ip_node) >=0 and stoi(ip_node) <=255) continue;
-        else return false;
-        return true;
+    for(int i=0; i<ip.size(); i++){
+        tempc.push_back(ip[i]);
     }
+
+    for(int i=0; i<tempc.size(); i++){
+        if(tempc[i]=='.') continue;
+        else if (isdigit(tempc[i])) continue;
+        else return false;
+    }
+
+    for(int i=0; i<=tempc.size(); i++){
+        if(tempc[i]=='.'){
+            ip_list.push_back(ip.substr(prev,i-prev));
+            prev = i+1;
+        }
+        if(ip[i] == '\0') ip_list.push_back(ip.substr(prev,i-prev));
+    }
+
+    for(int j=0; j<4; j++){
+        if( (stoi(ip_list[j]) >=0) && (stoi(ip_list[j]) <=255) ) continue;
+        else return false;
+    }
+    return true;
+    
 }
 
 // Main function
