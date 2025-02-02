a83d65e794bb750d1173a40f5e838417f801a122
Sachindu Malshan <cst19035@std.uwu.ac.lk>
2025-02-02 16:37:38 +0000

    Solve the bugs in the code

--- src/09/Challenge/ch9_fix.cpp
+++ src/09/Challenge/ch9_fix.cpp
@@ -1,28 +1,22 @@
-// C++ Code Challenges, LinkedIn Learning
-
-// Challenge #9: Fixing Faulty Code
-// Fix the bugs in the provided code.
-// The application is supposed to ask the user for a string, and print the number of vowels contained in the string.
-
-// For example: 
-// "I love my dog" has 4 vowels, "education" has 5 vowels, and "Sly spy by my crypt" has 0 vowels.
-
-#include <string>
+#include <iostream>
 
 // vowel_count()
 // Summary: This function counts the number of vowels in an incoming string.
-// Arguments:
-//           str: The string to analyze.
 // Returns: An integer with the vowel count.
 int vowel_count(const std::string str){
     int count=0;
-    for(int i; i<str.length(); i++)
+    for(int i=0; i<str.length(); i++)
         switch(str[i]){
             case 'a':
             case 'e':
             case 'i':
             case 'o':
             case 'u':
+            case 'A':
+            case 'E':
+            case 'I':
+            case 'O':
+            case 'U':
             count++;
         }
     return count;
@@ -30,10 +24,9 @@ int vowel_count(const std::string str){
 
 // Faulty code, main()
 // Summary: This application ask the user for a string, and prints the number of vowels in it.
-void main(){
+int main(){
     std::string input; 
     std::cout << "Enter some text: " << std::flush;
-    std::cin >> input;
+    getline(std::cin,input);
     std::cout << "\nThe text you entered contains " << vowel_count(input) << " vowels.\n\n";
 }
- 
