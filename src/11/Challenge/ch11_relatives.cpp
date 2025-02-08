d44d6fc6d3baff4523fd71542c3d32c86fc40cde
Sachindu Malshan <cst19035@std.uwu.ac.lk>
2025-02-08 19:44:16 +0000

    Initially problem was solved

--- src/11/Challenge/ch11_relatives.cpp
+++ src/11/Challenge/ch11_relatives.cpp
@@ -15,12 +15,26 @@
 // Finding Relatives, main()
 // Summary: This application displays the first pair of possible relatives from a list of names in a CSV file.
 int main(){
+    std::string str;
+    std::vector <std::string> names;
+    std::vector <std::string> names2;
+    std::vector <std::string> finall;
     std::fstream file ("names.csv", std::ios::in);
     // Read the CSV file.
     if(file.is_open()){
 
-        // Write your code here
-    
+        while(!file.eof()){
+            getline(file,str);
+            //std::cout << str << std::endl;
+        }
+
+        std::stringstream ss(str);
+
+        while(ss.good()){
+            std::string substr;
+            getline(ss,substr,',');
+            names.push_back(substr);
+        }    
         file.close();
     }
     else{
@@ -28,9 +42,51 @@ int main(){
         return 1;
     }
 
-    // Write your code here
+    // for(int i=0;i<names.size();i++){
+    //     std::cout << names[i] << std::endl;
+    // }
 
-    std::cout << "No relatives found.\n\n" << std::flush;    
+    for(int i=0;i<names.size();i++){
+        std::stringstream ss(names[i]);
+        while(ss.good()){
+            std::string substr;
+            getline(ss,substr,' ');
+            names2.push_back(substr);
+        }
+    }
+
+    // for(int i=0;i<names2.size();i++){
+    //     std::cout << names2[i] << std::endl;
+    // }
+
+    for(int i=1;i<names2.size();i+=2){
+        for(int j=i+2;j<names2.size();j+=2){
+            if(names2[i]==names2[j]){
+                finall.push_back(names2[i-1] + " " + names2[i]);
+                finall.push_back(names2[j-1] + " " + names2[j]);
+                break;
+            }
+        }
+    }
+
+    // for(int i=1;i<names2.size();i=+2){
+    //      for(int j=i+2;j<names2.size();j=+2){
+    //         if(names2[i]==names2[j]){
+    //             std::string newname1 = names2[i-1] + " " + names2[i];
+    //             std::string newname2 = names2[j-1] + " " + names2[j];
+    //             finall.push_back(newname1);
+    //             finall.push_back(newname2);
+    //             break;
+    //         }
+    //      }
+    // }
+
+    // for(int i=0;i<finall.size();i++){
+    //     std::cout << finall[i] << std::endl;
+    // }
+
+    if(finall.size() > 0) std::cout << finall[0] << "," << finall[1] << std::endl;
+    else std::cout << "No relatives found.\n\n" << std::flush;    
     return 0;
 }
  
