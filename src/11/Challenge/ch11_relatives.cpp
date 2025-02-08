e49444faac555e9957b05486d513dd959de5a771
Sachindu Malshan <cst19035@std.uwu.ac.lk>
2025-02-08 19:53:11 +0000

    Optimize the code

--- src/11/Challenge/ch11_relatives.cpp
+++ src/11/Challenge/ch11_relatives.cpp
@@ -16,16 +16,16 @@
 // Summary: This application displays the first pair of possible relatives from a list of names in a CSV file.
 int main(){
     std::string str;
-    std::vector <std::string> names;
-    std::vector <std::string> names2;
-    std::vector <std::string> finall;
+    std::vector <std::string> double_name;
+    std::vector <std::string> single_name;
+    std::vector <std::string> name_pairs;
     std::fstream file ("names.csv", std::ios::in);
+    
     // Read the CSV file.
     if(file.is_open()){
 
         while(!file.eof()){
             getline(file,str);
-            //std::cout << str << std::endl;
         }
 
         std::stringstream ss(str);
@@ -33,7 +33,7 @@ int main(){
         while(ss.good()){
             std::string substr;
             getline(ss,substr,',');
-            names.push_back(substr);
+            double_name.push_back(substr);
         }    
         file.close();
     }
@@ -42,50 +42,26 @@ int main(){
         return 1;
     }
 
-    // for(int i=0;i<names.size();i++){
-    //     std::cout << names[i] << std::endl;
-    // }
-
-    for(int i=0;i<names.size();i++){
-        std::stringstream ss(names[i]);
+    for(int i=0;i<double_name.size();i++){
+        std::stringstream ss(double_name[i]);
         while(ss.good()){
             std::string substr;
             getline(ss,substr,' ');
-            names2.push_back(substr);
+            single_name.push_back(substr);
         }
     }
 
-    // for(int i=0;i<names2.size();i++){
-    //     std::cout << names2[i] << std::endl;
-    // }
-
-    for(int i=1;i<names2.size();i+=2){
-        for(int j=i+2;j<names2.size();j+=2){
-            if(names2[i]==names2[j]){
-                finall.push_back(names2[i-1] + " " + names2[i]);
-                finall.push_back(names2[j-1] + " " + names2[j]);
+    for(int i=1;i<single_name.size();i+=2){
+        for(int j=i+2;j<single_name.size();j+=2){
+            if(single_name[i]==single_name[j]){
+                name_pairs.push_back(single_name[i-1] + " " + single_name[i]);
+                name_pairs.push_back(single_name[j-1] + " " + single_name[j]);
                 break;
             }
         }
     }
 
-    // for(int i=1;i<names2.size();i=+2){
-    //      for(int j=i+2;j<names2.size();j=+2){
-    //         if(names2[i]==names2[j]){
-    //             std::string newname1 = names2[i-1] + " " + names2[i];
-    //             std::string newname2 = names2[j-1] + " " + names2[j];
-    //             finall.push_back(newname1);
-    //             finall.push_back(newname2);
-    //             break;
-    //         }
-    //      }
-    // }
-
-    // for(int i=0;i<finall.size();i++){
-    //     std::cout << finall[i] << std::endl;
-    // }
-
-    if(finall.size() > 0) std::cout << finall[0] << "," << finall[1] << std::endl;
+    if(name_pairs.size() > 0) std::cout << name_pairs[0] << "," << name_pairs[1] << std::endl;
     else std::cout << "No relatives found.\n\n" << std::flush;    
     return 0;
 }
