7762d34141f680ca4a4b63c4dff44b18a655b8e2
Sachindu Malshan <cst19035@std.uwu.ac.lk>
2025-02-15 09:44:58 +0000

    Initial version implement for curly brackets

--- src/15/Challenge/ch15_json.cpp
+++ src/15/Challenge/ch15_json.cpp
@@ -12,25 +12,59 @@
 #include <string>
 #include <stack>
 
+
 // is_valid_JSON()
 // Summary: This function returns true if the file in the argument is a valid JSON file based on its balance of braces, brackets, and quotes.
 // Arguments:
 //           filename: A string with the name of the file to open.
-// Returns: An integer enconding: 1 for valid JSON files, 0 for invalid, and -1 on failure to open file. 
+// Returns: An integer enconding: 1 for valid JSON files, 0 for invalid, and -1 on failure to open file.
+
+std::stack <std::string> sentences;
+std::stack <char> signs;
+
+
 int is_valid_JSON(std::string filename){
     std::string line;
-    bool quotes = false; 
+    bool quotes = false;
 
     std::fstream file (filename, std::ios::in);
     if(file.is_open()){
  
-        // Write your code here
-
+        while(!file.eof()){
+            getline(file,line);
+            //std::cout << line << std::endl;
+            sentences.push(line);
+        }        
         file.close();
     }
     else
         return -1;
+    
+    int cv = sentences.size();
+    for(int i=0;i<cv;i++){
+        line = sentences.top();
+        sentences.pop();
+        for(int j=0; j<line.length();j++){
+            if(!signs.empty()){
+                // std::cout << signs.top() << std::endl;
+                char temp = signs.top();
+                char temp2 = line[j];
+                if(temp == temp2){
+                    signs.push(temp2);
+                }
+                else if(temp2 == '}' || temp2 == '{') signs.pop();
+                else continue;
+            }
+            else{
+                if(line[j] == '}' || line[j] == '{')
+                    signs.push(line[j]);
+            }
+        }        
+    }
+    std::cout << signs.size() << std::endl;
 }
+
+//|| line[j] == '[' || line[j] == ']' || line[j] == '"'
 
 // JSON File Validation, main()
 int main(){
